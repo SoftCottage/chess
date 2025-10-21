3a9c7f8b81dfb3a986ff7c6a2289f9087890c6d1
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-10-21 17:04:42 -0600

    Add DataAccess class along with data clearing.

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataAccess {
+    private final Map<String, Object> users = new HashMap<>();
+    private final Map<String, Object> games = new HashMap<>();
+    private final Map<String, Object> auths = new HashMap<>();
+
+    public void clearDatabase() throws DataAccessException {
+        try {
+            users.clear();
+            games.clear();
+            auths.clear();
+        } catch (Exception ex) {
+            throw new DataAccessException("Error clearing database", ex);
+        }
+    }
+}
