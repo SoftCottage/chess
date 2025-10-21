5b59bed66d9ae61a49c57c9fbfb93809111c8c6d
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-10-21 17:18:48 -0600

    Update Server to access Clear Classes.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
 import io.javalin.*;
+import handler.ClearHandler;
+import service.ClearService;
+import dataaccess.DataAccess;
 
 public class Server {
 
@@ -10,7 +13,11 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        var dataAccess = new DataAccess();
+        var clearService = new ClearService(dataAccess);
+        var clearHandler = new ClearHandler(clearService);
 
+        javalin.delete("/db", clearHandler::handle);
     }
 
     public int run(int desiredPort) {
