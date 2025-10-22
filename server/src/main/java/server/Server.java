583b4f506f5f1f183497252541bed2c88ad82d47
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-10-21 21:46:20 -0600

    Add Register endpoint to Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,22 +2,33 @@ package server;
 
 import io.javalin.Javalin;
 import handler.ClearHandler;
+import handler.RegisterHandler;
 import service.ClearService;
+import service.RegisterService;
 import dataaccess.DataAccess;
 
 public class Server {
 
     private final Javalin javalin;
+    private final DataAccess dataAccess;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        var dataAccess = new DataAccess();
-        var clearService = new ClearService(dataAccess);
-        var clearHandler = new ClearHandler(clearService);
+        // Single shared DataAccess for all services
+        dataAccess = new DataAccess();
 
         // Clear endpoint
+        var clearService = new ClearService(dataAccess);
+        var clearHandler = new ClearHandler(clearService);
         javalin.delete("/db", clearHandler::handle);
+
+        // Register endpoint
+        var registerService = new RegisterService(dataAccess);
+        var registerHandler = new RegisterHandler(registerService);
+        javalin.post("/user", registerHandler::handle);
+
+        // future: login, session delete, /game, etc.
     }
 
     public int run(int desiredPort) {
