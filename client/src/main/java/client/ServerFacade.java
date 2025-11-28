bad13c30e197faf26fc86e0704cefff4f617b38b
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-11-28 15:12:06 -0700

    Add ServerFacade skeleton

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,17 @@
+package client;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    // TODO: register
+    // TODO: login
+    // TODO: logout
+    // TODO: createGame
+    // TODO: listGames
+    // TODO: joinGame
+}
