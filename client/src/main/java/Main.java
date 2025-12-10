78cd5bb7e5179cf66463e47b8268bc137940c8da
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-12-10 09:37:39 -0700

    Fix folders

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,10 +4,8 @@ import ui.ChessClient;
 public class Main {
     public static void main(String[] args) {
 
-        // Default server URL (your server will run on this)
         String serverUrl = "http://localhost:8080";
 
-        // If run with a command-line argument, override default
         if (args.length == 1) {
             serverUrl = args[0];
         }
@@ -17,6 +15,6 @@ public class Main {
         ChessClient client = new ChessClient(facade);
 
         System.out.println("♕ 240 Chess Client");
-        client.run();  // Start the REPL loop
+        client.run();
     }
 }
